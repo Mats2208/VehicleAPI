23c01d827af8a34ef38115086ea40439b837465a
Mateo <mateoandres2208@gmail.com>
2025-09-01 20:11:39 -0400

    Tarea terminada

--- VehicleAPI/obj/Debug/net9.0/VehicleAPI.AssemblyInfo.cs
+++ VehicleAPI/obj/Debug/net9.0/VehicleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12dd65152ef5e212293dc76e1e2c98c464400dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cd117e955eede3acdc5a23912ea2a1b7ba420d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
